b594efb9a1b139d050b24ffe6eabc7cff7c7d7aa
Yves Sablonier <git@sablonier.ch>
2017-02-20 00:48:09 +0100

    Revert "testing slickslider"
    This reverts commit 8b88b9bf2686fd18fe055cd03097250fe52099b3.

--- src/CarouselExtension.php
+++ src/CarouselExtension.php
@@ -2,10 +2,7 @@
 
 namespace sablonier\carousel;
 
-use Bolt\Asset\File\Stylesheet;
-use Bolt\Asset\File\JavaScript;
 use Bolt\Extension\SimpleExtension;
-use Bolt\Controller\Zone;
 
 class CarouselExtension extends SimpleExtension
 {
@@ -17,17 +14,6 @@ class CarouselExtension extends SimpleExtension
     {
         return [
             'carousel'    => ['carouselFunction']
-        ];
-    }
-    
-    /**
-     * {@inheritdoc}
-     */
-    protected function registerAssets()
-    {
-        return [
-            new JavaScript('extensions/vendor/sablonier/carousel/slick/slick.js'),
-            new Stylesheet('extensions/vendor/sablonier/carousel/slick/slick.css'),
         ];
     }
 
@@ -62,9 +48,9 @@ class CarouselExtension extends SimpleExtension
             case 'foundation6':
                 $carousel = $this->renderTemplate('_carousel_foundation6.twig', $context);
                 break;
-            case 'slickslider':
-                $carousel = $this->renderTemplate('_carousel_slickslider.twig', $context);
-                break;
+            /*case 'purecss':
+                $carousel = $this->renderTemplate('_carousel_purecss.twig', $context);
+                break;*/
         }
 
         echo $carousel;
